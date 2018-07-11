17ab6c94a9866bd08144b3db9f8d2f0dcf6f0ca8
RodrigoMCarvalho <rodrigo_moreira84@hotmail.com>
2018-07-10 21:49:03 -0300

    configurado o acesso ao banco de dados

---
+++ src/br/com/caelum/livraria/dao/AutorDao.java
@@ -0,0 +1,36 @@
+package br.com.caelum.livraria.dao;
+
+import java.util.List;
+
+import javax.annotation.PostConstruct;
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+import br.com.caelum.livraria.modelo.Autor;
+
+@Stateless
+public class AutorDao {
+
+	@PersistenceContext
+	private EntityManager manager;
+	
+	@PostConstruct
+	public void aposCriacao() {
+		System.out.println("AutorDAO criado!");
+	}
+
+	public void salva(Autor autor) {
+		manager.persist(autor);
+	}
+	
+	public List<Autor> todosAutores() {
+		return manager.createQuery("SELECT a FROM Autor a", Autor.class).getResultList();
+	}
+
+	public Autor buscaPelaId(Integer autorId) {
+		Autor autor = manager.find(Autor.class, autorId);
+		return autor;
+	}
+	
+}
